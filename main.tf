9cccea4c6c49a53bef0f5631861f5e805c41f12d
System Administrator <root@macbook85.local>
2022-05-02 20:00:45 -0400

    commit

--- main.tf
+++ main.tf
@@ -22,11 +22,16 @@ variable "tenant_id" {
   type    = string
 }
 
+variable "client_secret" {
+  type    = string
+}
+
 provider "azurerm" {
   version = "~> 3.0.2"
   subscription_id = var.subscription_id
   client_id = var.client_id
   tenant_id = var.tenant_id
+  client_secret = var.client_secret
   features {}
 }
 
